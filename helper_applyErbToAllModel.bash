dff17d53f97610ec3c34f201d4da2be84a2ace2c
Jiang@Conti <plr258@gmail.com>
2021-11-18 12:10:51 +0900

    0.92

--- helper_applyErbToAllModel.bash
+++ helper_applyErbToAllModel.bash
@@ -1,13 +1,10 @@
-
 for dirName in $(ls); do
-  if $(find $dirName -name 'model.rsdf')
+  if pathToRsdf="./$(find $dirName -name 'model.rsdf')"
   then
-    echo $(find $dirName -name 'model.rsdf')
-    # erb $pathToRsdf > ./$dirName/model.sdf
+    erb $pathToRsdf > ./$dirName/model.sdf
   fi
 done
 
 for erbWorldFile in $(ls *.world.erb); do
-  echo "./${erbWorldFile%.erb}"
-  # erb ./$erbWorldFile > ./${erbWorldFile%.erb}
+  erb ./$erbWorldFile > ./${erbWorldFile%.erb}
 done
