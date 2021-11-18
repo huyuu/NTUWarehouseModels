dc2e37a2f3c0ec86a24f0d388c22c0b1201a74b9
Jiang@Conti <plr258@gmail.com>
2021-11-18 13:38:39 +0900

    0.97

--- helper_applyErbToAllModel.bash
+++ helper_applyErbToAllModel.bash
@@ -2,11 +2,12 @@ for dirName in $(ls -p); do
   pathToRsdf=$(find $dirName -name 'model.rsdf')
   if [ -n "$pathToRsdf" ]
   then
-    echo "\$pathToRsdf = $pathToRsdf"
     erb $pathToRsdf > ./$dirName/model.sdf
+    echo "converted $pathToRsdf to $dirName/model.sdf"
   fi
 done
 
 for erbWorldFile in $(ls *.world.erb); do
   erb ./$erbWorldFile > ./${erbWorldFile%.erb}
+  echo "converted $erbWorldFile to ${erbWorldFile%.erb}"
 done
