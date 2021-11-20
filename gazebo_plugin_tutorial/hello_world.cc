a9912c47fc53af4dca9231e63ee9a262ca7f691f
Jiang <uie27589@contiwan.com>
2021-11-20 14:36:31 +0900

    0.102

--- gazebo_plugin_tutorial/hello_world.cc
+++ gazebo_plugin_tutorial/hello_world.cc
@@ -1,17 +1,18 @@
 #include <gazebo/gazebo.hh>
 
-namespace gazebo
-{
-  class WorldPluginTutorial : public WorldPlugin
-  {
-    public: WorldPluginTutorial() : WorldPlugin()
-            {
-              printf("Hello World!\n");
-            }
-
-    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
+namespace gazebo {
+  class WorldPluginTutorial: public WorldPlugin {
+  public:
+    WorldPluginTutorial(): WorldPlugin() {
+      printf("Hello World!\n");
+    }
+    virtual ~WorldPluginTutorial () = default;
+    void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) override;
   };
-  GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial);
+
+
+  void WorldPluginTutorial::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) {
+    cout << "loaded" << endl;
+  }
 }
