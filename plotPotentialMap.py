a6ff16c9f5df98805b6649b25de6e37273e8b29c
Jiang <uie27589@contiwan.com>
2021-11-23 14:19:54 +0900

    0.136

--- plotPotentialMap.py
+++ plotPotentialMap.py
@@ -6,13 +6,14 @@ from mpl_toolkits.mplot3d import Axes3D
 
 potentialMap = pd.read_csv("./potentialMap.csv", header=None)
 potentialMap.columns = ["x", "y", "potential"]
+xs, ys = np.meshgrid(potentialMap['x'], potentialMap['y'])
 print(potentialMap)
 potentialMap = potentialMap.pivot(index='x', columns='y', values='potential')
 
-fig = plt.figure()
+fig = pl.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel("x", size = 16, color = "b")
 ax.set_ylabel("y", size = 16, color = "b")
 ax.set_zlabel("potential", size = 16, color = "b")
-ax.plot_surface(potentialMap.index, potentialMap.columns, potentialMap.values, cmap = "plasma_r")
+ax.plot_surface(xs, ys, potentialMap.values, cmap = "plasma_r")
 pl.show()
