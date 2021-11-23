6cded51c711cc8f7bc40d155e70c42cbbefbf8b2
Jiang <uie27589@contiwan.com>
2021-11-23 09:23:52 +0900

    0.110

---
+++ ActorAvoidingObstaclesPlugin/ActorAvoidingObstaclesPlugin.hh
@@ -0,0 +1,93 @@
+#ifndef MY_PLUGINS_ACTORAVOIDINGOBSTACLESPLUGIN_HH_
+#define MY_PLUGINS_ACTORAVOIDINGOBSTACLESPLUGIN_HH_
+
+#include <string>
+#include <vector>
+
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/util/system.hh"
+#include "ExpandingPotentialFieldPathPlanner.hh"
+
+
+namespace gazebo {
+  class GZ_PLUGIN_VISIBLE ActorAvoidingObstaclesPlugin : public ModelPlugin {
+  public: // Public Functions
+    // Constructor
+    ActorAvoidingObstaclesPlugin();
+
+    /// \brief Load the actor plugin.
+    /// \param[in] _model Pointer to the parent model.
+    /// \param[in] _sdf Pointer to the plugin's SDF elements.
+    void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
+
+    // Documentation Inherited.
+    void Reset() override;
+
+
+  private: // Private Functions
+    /// \brief Function that is called every update cycle.
+    /// \param[in] _info Timing information
+    virtual void OnUpdate(const common::UpdateInfo &_info);
+
+    /// \brief Helper function to choose a new target location
+    virtual void ChooseNewTarget();
+
+    /// \brief Helper function to avoid obstacles. This implements a very
+    /// simple vector-field algorithm.
+    /// \param[in] _pos Direction vector that should be adjusted according
+    /// to nearby obstacles.
+    virtual void HandleObstacles(ignition::math::Vector3d &_pos);
+
+
+    // Private Properties
+
+    /// \brief Pointer to the parent actor.
+    physics::ActorPtr actor;
+
+    /// \brief Pointer to the world, for convenience.
+    physics::WorldPtr world;
+
+    /// \brief Pointer to the sdf element.
+    sdf::ElementPtr sdf;
+
+    /// \brief Velocity of the actor
+    ignition::math::Vector3d velocity;
+
+    /// \brief List of connections
+    std::vector<event::ConnectionPtr> connections;
+
+    /// \brief Current target location
+    ignition::math::Vector3d target;
+
+    // vector which the actor is going
+    ignition::math::Vector3d goingVector;
+
+    /// \brief Target location weight (used for vector field)
+    double targetWeight {1.0};
+
+    /// \brief Obstacle weight (used for vector field)
+    double obstacleWeight {1.0};
+
+    /// \brief Time scaling factor. Used to coordinate translational motion
+    /// with the actor's walking animation.
+    double animationFactor {1.0};
+
+    /// \brief Time of the last update.
+    common::Time lastUpdate;
+
+    /// \brief List of models to ignore. Used for vector field
+    std::vector<std::string> ignoreModels;
+
+    /// \brief Custom trajectory info.
+    physics::TrajectoryInfoPtr trajectoryInfo;
+
+    // the custom path planner
+    ExpandingPotentialFieldPathPlanner pathPlanner {};
+
+    ignition::math::Box outerMostBoundaryBox {};
+  }
+}
+
+
+#endif
---
+++ ActorAvoidingObstaclesPlugin/ExpandingPotentialFieldPathPlanner.hh
@@ -0,0 +1,77 @@
+#ifndef ALGORITHM_EXPANDINGPOTENTIALFIELDPATHPLANNER_HH_
+#define ALGORITHM_EXPANDINGPOTENTIALFIELDPATHPLANNER_HH_
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/util/system.hh"
+#include <ignition/math.hh>
+
+using std::vector
+
+
+namespace gazebo {
+  class GZ_PLUGIN_VISIBLE ExpandingPotentialFieldPathPlanner {
+  public:
+    /// constructor
+    ExpandingPotentialFieldPathPlanner();
+    ~ExpandingPotentialFieldPathPlanner();
+    /// update models when there is any change
+    void updateModels(const ignition::math::Box, const physics::World&, const std::vector<std::string>&);
+    /// calculate vector for next step
+    virtual ignition::math::Vector2d generateGradientNearPosition(const ignition::math::Vector3d&) const;
+
+
+  private:
+    // Member Functions
+    /// calculate potential by Gauss Integral using the specific formula
+    double __calculatePotentialUsingFormula(const double& x, const double& y, const double& X_down, const double& X_up, const double& Y_down, const double& Y_up) const;
+    /// generate potential at point
+    double __generatePotentialAtPoint(const ignition::math::Vector3d&) const;
+
+    // Properties
+    /// actor bounding box
+    ignition::math::Box actorBoundingBox;
+    /// obstacles bounding boxes
+    vector<ignition::math::Box> obstacleBoundingBoxes;
+    /// target position
+    ignition::math::Vector3d targetPosition {};
+    /// sample amount
+    int sampleAmount {100};
+    /// potential map
+    vector<vector<double>> potentialMap(sampleAmount, vector<double>(sampleAmount, 0.0));
+    /// gauss points and weights
+    // reference: https://pomax.github.io/bezierinfo/legendre-gauss.html
+    static const int gaussSampleAmount = 10;
+    static const vector<double> gaussPoints {
+      -0.1488743389816312,
+      0.1488743389816312,
+      -0.4333953941292472,
+      0.4333953941292472,
+      -0.6794095682990244,
+      0.6794095682990244,
+      -0.8650633666889845,
+      0.8650633666889845,
+      -0.9739065285171717,
+      0.9739065285171717
+    };
+    static const vector<double> gaussWeights {
+      0.2955242247147529,
+      0.2955242247147529,
+      0.2692667193099963,
+      0.2692667193099963,
+      0.2190863625159820,
+      0.2190863625159820,
+      0.1494513491505806,
+      0.1494513491505806,
+      0.0666713443086881,
+      0.0666713443086881
+    };
+    /// small delta h for numerous differential
+    static constexpr double h {0.01}; // 1[cm]
+  };
+}
+
+#endif
