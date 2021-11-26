41032cb958443acf4d1ffec23750a006b9f85403
Jiang@Conti <plr258@gmail.com>
2021-11-26 15:14:04 +0900

    0.211

--- ActorAvoidingObstaclesPlugin/ActorAvoidingObstaclesPlugin.cc
+++ ActorAvoidingObstaclesPlugin/ActorAvoidingObstaclesPlugin.cc
@@ -62,7 +62,7 @@ void ActorAvoidingObstaclesPlugin::Load(physics::ModelPtr _model, sdf::ElementPt
   }
 
   // handle pathPlanner related stuffs
-  this->pathPlanner = std::move(AStarPathPlanner(his->actor->WorldPose().Pos(), this->target, this->actor->BoundingBox(), this->world, this->ignoreModels));
+  this->pathPlanner = std::move(AStarPathPlanner(this->actor->WorldPose().Pos(), this->target, this->actor->BoundingBox(), this->world, this->ignoreModels));
   // this->pathPlanner.lazyConstructor(this->actor->WorldPose().Pos(), this->target);
   // this->pathPlanner.updateModels(this->actor->BoundingBox(), this->world, this->ignoreModels);
 
