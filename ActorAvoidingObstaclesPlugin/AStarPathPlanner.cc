8462808ba01d3453cef2788e5809c05c7454daf5
Jiang@Conti <plr258@gmail.com>
2021-11-27 18:50:29 +0900

    0.250

--- ActorAvoidingObstaclesPlugin/AStarPathPlanner.cc
+++ ActorAvoidingObstaclesPlugin/AStarPathPlanner.cc
@@ -138,12 +138,12 @@ void AStarPathPlanner::__addNodesNearToOpenList(const Node& currentNode) {
       std::cout << "inserting potential node: " << potentialNode.position.X() << ", " << potentialNode.position.Y() << " Into nodes." << std::endl;
       const int nodeCounter = this->nodes.size();
       const Node* currentNodePtr = &currentNode;
-      this->nodes.push_back(Node{
+      this->nodes.push_back(Node(
         nodeCounter,// id
         currentNodePtr,// parent node
         potentialNode.position,// position
         this->target,// target
-      });
+      ));
       std::cout << "currentNode(1) = " << currentNode << std::endl;
       Node* newNodePtr = &(this->nodes.back());
       std::cout << "currentNode(2) = " << currentNode << "with newNode's parent: " << *(newNodePtr->parentNodePtr) <<  std::endl;
