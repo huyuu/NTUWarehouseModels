5a68cd6f628a936d49428682409f9c0319cd1e9f
Jiang <uie27589@contiwan.com>
2021-11-30 16:17:41 +0900

    0.378

--- ActorAvoidingObstaclesPlugin/AStarPathPlanner.cc
+++ ActorAvoidingObstaclesPlugin/AStarPathPlanner.cc
@@ -39,6 +39,9 @@ AStarPathPlanner::AStarPathPlanner(ignition::math::Vector3d start, ignition::mat
   const unsigned int modelCount {world->ModelCount()};
   for (unsigned int i = 0; i < modelCount; ++i) {
     std::cout << "for model: " << i << std::endl;
+    if (i >= 155) {
+      break;
+    }
     const physics::ModelPtr model = world->ModelByIndex(i);
     if (std::find(ignoreModels.begin(), ignoreModels.end(), model->GetName()) == ignoreModels.end()) {
       ignition::math::AxisAlignedBox boundingBox = world->ModelByIndex(i)->CollisionBoundingBox();
